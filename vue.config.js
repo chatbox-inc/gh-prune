14881ff007a346283ffcf09a75e2dd7d2f931e65
mikakane <t.goto@chatbox-inc.com>
2019-02-23 15:18:27 +0900

    update

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new CopyWebpackPlugin([
+        {
+          from: './static/',
+          to: './'
+        }
+      ])
+    ]
+  }
+}
